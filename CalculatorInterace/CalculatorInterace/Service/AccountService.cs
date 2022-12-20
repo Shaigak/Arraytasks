1c14ccd535b00b1ea3d57fe586d888c35f8e64b1
Shaig Khazimov <119400387+Shaigak@users.noreply.github.com>
2022-12-20 18:10:53 +0400

    Add files via upload

---
+++ CalculatorInterace/CalculatorInterace/Service/AccountService.cs
@@ -0,0 +1,45 @@
+﻿using CalculatorInterace.Service.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorInterace.Service
+{
+    internal class AccountService:IAccountService
+    {
+         
+        public string name;
+        public string password;
+
+        public AccountService(string name, string password)
+        {
+            this.name = name;
+            this.password = password;
+        }
+
+        public void sign()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void Sign()
+        {
+            if (name == "Cavid123" && password == "Cavid1993")
+            {
+                Console.WriteLine("Ugurludur");
+            }
+            else
+            {
+                Console.WriteLine("Username ve ya password yanlisdir");
+            }
+        }
+
+        void IAccountService.AccountService()
+        {
+            
+        }
+    }
+}
+ 
---
+++ CalculatorInterace/CalculatorInterace/Service/Interface/IAccountService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorInterace.Service.Interface
+{
+    internal interface IAccountService
+    {
+        void AccountService();
+        void sign();
+    }
+}
